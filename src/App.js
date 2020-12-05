6343672f690e1cf6b796eb9d649eaadc33ea9408
ayeshamemon <67019119+ayeshamemon@users.noreply.github.com>
2020-12-05 21:45:54 +0530

    task done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import { markers } from "./data";
+const mapStyles = {
+  width: "100%",
+  height: "100%",
+};
 
-function App() {
+export const App = (props) => {
+  const [showingInfoWindow, setShowingInfoWindow] = useState(false);
+  const [activeMarker, setActiveMarker] = useState({});
+  const [selectedPlace, setSelectedPlace] = useState({});
+
+  const onMarkerClick = (props, marker, e) => {
+    setSelectedPlace(props);
+    setActiveMarker(marker);
+    setShowingInfoWindow(true);
+  };
+
+  const onClose = (props) => {
+    if (showingInfoWindow) {
+      setShowingInfoWindow(false);
+      setActiveMarker(null);
+    }
+  };
+
+  console.log(props.google);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map
+      google={props.google}
+      zoom={14}
+      style={mapStyles}
+      // initialCenter={{
+      //   lat: -1.2884,
+      //   lng: 36.8233,
+      // }}
+    >
+      {markers.map((marker, key) => {
+        console.log(marker);
+        return (
+          <Marker
+            icon={{
+              url: marker.icon,
+            }}
+            onClick={onMarkerClick}
+            name={marker.name}
+            description={marker.description}
+            position={{ lat: marker.lat, lng: marker.lng }}
+          />
+        );
+      })}
+      {/*
+        <Marker
+          onClick={this.onMarkerClick}
+          name={"Kenyatta International Convention Centre"}
+          description="ABCSSSS"
+          position={{ lat: 37.778519, lng: -122.40564 }}
+        />*/}
+      <InfoWindow
+        marker={activeMarker}
+        visible={showingInfoWindow}
+        onClose={onClose}
+      >
+        <div>
+          <h4 style={{ textAlign: "center" }}>{selectedPlace.name}</h4>
+          <p style={{ textAlign: "center", fontWeight: "bold" }}>
+            {selectedPlace.description}
+          </p>
+        </div>
+      </InfoWindow>
+    </Map>
   );
-}
+};
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCmS7UQZi75sX36V-CTdjzhRgz7SW2ho4g",
+})(App);
---
+++ src/data.js
@@ -0,0 +1,21 @@
+import iconCity from "./img/icons8-city-48.png";
+import iconFood from "./img/icons8-gingerbread-house-48.png";
+import iconShopping from "./img/icons8-shopping-basket-64.png";
+export const markers = [
+  {
+    name: "Kenyatta International Convention Centre",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    lat: 37.778519,
+    lng: -122.40564,
+    icon: iconCity,
+  },
+  {
+    name: "Kenkom House",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    lat: 37.762391,
+    lng: -122.439192,
+    icon: iconFood,
+  },
+];
